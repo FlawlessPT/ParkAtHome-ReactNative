55d31ddc97efbd9fe1d327d78343e62d30dbc80a
FlawlessPT <jotape919@gmail.com>
2021-02-12 00:34:16 +0000

    Update database file only

--- src/constant/database.js
+++ src/constant/database.js
@@ -1,5 +1,5 @@
 export const connection = {
-  url: "http://192.168.1.11:80", //computers ipv4 address
+  url: "http://192.168.1.16:80", //computers ipv4 address
   directory: "/ParkAtHome-Back_end",
 };
 
